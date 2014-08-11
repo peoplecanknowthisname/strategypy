8062cc4b398e385a260ddf1b21ebb5ea54b04c50
Robert Boston <Robert.Boston@hogarthww.com>
2014-08-11 20:30:44 +0100

    capture a bit more information about who kills who (cherry picked from commit 1e54aa2fe8be9a17fdc203352d5ec9f549b09c59)
    Conflicts:
    
    	strategypy/simplefe.py

--- strategypy/simplefe.py
+++ strategypy/simplefe.py
@@ -11,31 +11,32 @@ def print_frames(output_dict):
         [' '] * grid_size[1]
         for i in range(grid_size[0])
     ]
-    
+
     print '-' * (grid_size[1] + 2)
-    
+
     for frame in output_dict['frames']:
         grid = copy.deepcopy(empty_grid)
         for player_pk in frame:
             for unit_id in frame[player_pk]:
                 x, y = frame[player_pk][unit_id]
                 grid[x][y] = player_pk
-    
+
         for row in grid:
             print '|'+''.join(row)+'|'
-        
-        print '-'*(grid_size[1]+2)         
+
+        print '-'*(grid_size[1]+2)
 
 
 def print_summary(output_dict):
     winner = output_dict['winner']
     players = output_dict['players']
-         
+    all_players = output_dict['all_players']
+
     turns = output_dict['turns']
-    
+
     initial_frame = output_dict['frames'][0]
     last_frame = output_dict['frames'][-1]
-    
+
     initial_count = sum((len(x) for x in initial_frame.itervalues()))
     final_count = sum((len(x) for x in last_frame.itervalues()))
 
@@ -46,8 +47,19 @@ def print_summary(output_dict):
         print 'Player {} won in {} turns'.format(winner, turns)
     print 'Initial unit count: {}'.format(initial_count)
     print 'Final unit count: {}'.format(final_count)
-    
-    
+
+    for player in all_players.values():
+        print 'Player {} killed: '.format(player['name']),
+        for killed_player, num_times in player['has_killed'].items():
+            print '{} x {}, '.format(killed_player, num_times),
+        print
+
+    for player in all_players.values():
+        print 'Player {} was killed by: '.format(player['name']),
+        for killed_player, num_times in player['was_killed_by'].items():
+            print '{} x {}, '.format(killed_player, num_times),
+        print
+
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-f", "--frames",
@@ -56,10 +68,9 @@ if __name__ == "__main__":
 
     (options, _) = parser.parse_args()
 
-
     output = sys.stdin.read()
     output_dict = json.loads(output)
-    
+
     if options.show_frames:
         print_frames(output_dict)
     print_summary(output_dict)
