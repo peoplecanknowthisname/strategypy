747cc953d6a09555cf2d95438ef1201eee6234fa
Robert Boston <robert.t.boston@gmail.com>
2014-08-23 09:46:45 +0100

    make happy bots a bit perameterisable

---
+++ strategypy/bots/happines.py
@@ -0,0 +1,15 @@
+import random
+import sys
+from copy import deepcopy
+
+from happines_base import Bot as HappinessBaseBot
+
+from api import BaseBot
+
+
+
+class Bot(HappinessBaseBot):
+    
+
+    def calc_happiness(self, friend_dist, enemy_dist):
+        return 0
